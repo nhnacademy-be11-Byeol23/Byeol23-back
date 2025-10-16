84e79293449b175fd996fe0758154902286354bf
박지완 <sat981023@gmail.com>
2025-10-16 17:19:16 +0900

    git action test

--- src/main/java/com/nhnacademy/byeol23backend/controller/TestController.java
+++ src/main/java/com/nhnacademy/byeol23backend/controller/TestController.java
@@ -1,12 +1,14 @@
 package com.nhnacademy.byeol23backend.controller;
 
 import com.nhnacademy.byeol23backend.dto.TestDTO;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/backend")
 public class TestController {
+    @GetMapping
     public TestDTO test(){
         return new TestDTO("test", 111);
     }
