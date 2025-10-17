f080c686019dc944aa74d661c91af989f12724a1
박지완 <sat981023@gmail.com>
2025-10-17 13:54:02 +0900

    add log

--- src/main/java/com/nhnacademy/byeol23backend/controller/TestController.java
+++ src/main/java/com/nhnacademy/byeol23backend/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.nhnacademy.byeol23backend.controller;
 
 import com.nhnacademy.byeol23backend.dto.TestDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
     @GetMapping("/backend")
     public TestDTO test(){
+        log.info("back");
         return new TestDTO("test", 111);
     }
 }
