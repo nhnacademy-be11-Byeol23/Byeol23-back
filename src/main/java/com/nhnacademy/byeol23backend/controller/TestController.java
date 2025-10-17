f6ee2c25971b4fe4f7a554df444662b4b457037a
박지완 <sat981023@gmail.com>
2025-10-17 14:17:04 +0900

    controller url update

--- src/main/java/com/nhnacademy/byeol23backend/controller/TestController.java
+++ src/main/java/com/nhnacademy/byeol23backend/controller/TestController.java
@@ -3,12 +3,14 @@ package com.nhnacademy.byeol23backend.controller;
 import com.nhnacademy.byeol23backend.dto.TestDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequestMapping("/backend")
 public class TestController {
-    @GetMapping("/backend")
+    @GetMapping("/test")
     public TestDTO test(){
         log.info("back");
         return new TestDTO("test", 111);
