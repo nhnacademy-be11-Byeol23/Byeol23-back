7188f7a2304559abf66cca00f2237bf6c58376a5
yuhyun choi <dbgus315@gmail.com>
2025-10-23 01:05:04 +0900

    feat: 결제 로직 구현 중

---
+++ src/main/java/com/nhnacademy/byeol23backend/orderset/order/domain/dto/OrderPrepareResponse.java
@@ -0,0 +1,5 @@
+package com.nhnacademy.byeol23backend.orderset.order.domain.dto;
+
+public record OrderPrepareResponse(String orderNumber) {
+
+}
--- src/main/java/com/nhnacademy/byeol23backend/orderset/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/nhnacademy/byeol23backend/orderset/order/service/impl/OrderServiceImpl.java
@@ -1,8 +1,32 @@
 package com.nhnacademy.byeol23backend.orderset.order.service.impl;
 
-import com.nhnacademy.byeol23backend.orderset.order.service.OrderService;
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
+import com.nhnacademy.byeol23backend.orderset.order.domain.Order;
+import com.nhnacademy.byeol23backend.orderset.order.domain.dto.OrderPrepareRequest;
+import com.nhnacademy.byeol23backend.orderset.order.domain.dto.OrderPrepareResponse;
+import com.nhnacademy.byeol23backend.orderset.order.repository.OrderRepository;
+import com.nhnacademy.byeol23backend.orderset.order.service.OrderService;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+	private final OrderRepository orderRepository;
+
+	@Override
+	public OrderPrepareResponse prepareOrder(OrderPrepareRequest request) {
+
+		Order order = new Order(request.orderNumber(), request.totalBookPrice(), request.actualOrderPrice(),
+			LocalDateTime.now(), "대기", LocalDateTime.now().plusDays(3).toLocalDate(), request.receiver(),
+			request.postCode(), request.receiverAddress(), request.receiverAddressDetail(), request.receiverPhone());
+
+		orderRepository.save(order);
+
+		return new OrderPrepareResponse(order.getOrderNumber());
+	}
+
 }
