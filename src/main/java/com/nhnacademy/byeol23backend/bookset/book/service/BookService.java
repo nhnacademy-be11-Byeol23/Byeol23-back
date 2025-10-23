92065f64683de13747ec87ebe22266bd752593f0
임찬혁 <dlacksgur7@naver.com>
2025-10-23 10:45:18 +0900

    feat: 도서 CRU구현

--- src/main/java/com/nhnacademy/byeol23backend/bookset/book/service/BookService.java
+++ src/main/java/com/nhnacademy/byeol23backend/bookset/book/service/BookService.java
@@ -1,4 +1,13 @@
 package com.nhnacademy.byeol23backend.bookset.book.service;
 
+import com.nhnacademy.byeol23backend.bookset.book.dto.BookCreateRequest;
+import com.nhnacademy.byeol23backend.bookset.book.dto.BookResponse;
+import com.nhnacademy.byeol23backend.bookset.book.dto.BookUpdateRequest;
+
 public interface BookService {
+	BookResponse createBook(BookCreateRequest createRequest);
+
+	BookResponse getBook(Long bookId);
+
+	BookResponse updateBook(Long bookId, BookUpdateRequest updateRequest);
 }
