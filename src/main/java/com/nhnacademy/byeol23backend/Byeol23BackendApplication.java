7b42fde85c4c8e3782a0aaff7c6671c9156e3bf5
박지완 <sat981023@gmail.com>
2025-10-31 16:54:29 +0900

    WIP:save

--- src/main/java/com/nhnacademy/byeol23backend/Byeol23BackendApplication.java
+++ src/main/java/com/nhnacademy/byeol23backend/Byeol23BackendApplication.java
@@ -3,6 +3,7 @@ package com.nhnacademy.byeol23backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Byeol23BackendApplication {
 	public static void main(String[] args) {
