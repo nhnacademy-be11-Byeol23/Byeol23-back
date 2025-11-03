360caa4f5502cabf6148134095e5b89a0f8ae97f
박지완 <sat981023@gmail.com>
2025-11-03 22:16:19 +0900

    feature:coupon-policy

--- src/main/java/com/nhnacademy/byeol23backend/couponset/couponpolicy/service/CouponPolicyService.java
+++ src/main/java/com/nhnacademy/byeol23backend/couponset/couponpolicy/service/CouponPolicyService.java
@@ -1,7 +1,12 @@
 package com.nhnacademy.byeol23backend.couponset.couponpolicy.service;
 
 import com.nhnacademy.byeol23backend.couponset.couponpolicy.domain.dto.CouponPolicyCreateRequest;
+import com.nhnacademy.byeol23backend.couponset.couponpolicy.domain.dto.CouponPolicyInfoResponse;
+
+import java.util.List;
 
 public interface CouponPolicyService {
     void createCouponPolicy(CouponPolicyCreateRequest couponPolicyCreateRequest);
+
+    List<CouponPolicyInfoResponse> getCouponPolicies();
 }
