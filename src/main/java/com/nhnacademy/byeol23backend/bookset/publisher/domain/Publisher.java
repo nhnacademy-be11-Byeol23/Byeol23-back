344c31f40a5945859aba38405b96b899de1c3f95
yuhyunchoi <166400487+yuhyunchoi@users.noreply.github.com>
2025-11-07 15:07:45 +0900

    Update Publisher.java

--- src/main/java/com/nhnacademy/byeol23backend/bookset/publisher/domain/Publisher.java
+++ src/main/java/com/nhnacademy/byeol23backend/bookset/publisher/domain/Publisher.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Getter
 @Table(name = "publishers")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Publisher {
