af15d4aac0a754595f14422669bc0406e72a149e
Jinsu Kim <wlstn3697@naver.com>
2025-11-10 15:33:02 +0900

    dbchange

--- src/main/java/com/nhnacademy/byeol23backend/pointset/pointhistories/device/PointDevice.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.byeol23backend.pointset.pointhistories.device;
-
-import com.nhnacademy.byeol23backend.pointset.pointhistories.domain.PointHistory;
-
-public interface PointDevice {
-	void setPointHistory(PointHistory pointHistory);
-}
--- src/main/java/com/nhnacademy/byeol23backend/pointset/pointhistories/service/impl/PointServiceImpl.java
+++ src/main/java/com/nhnacademy/byeol23backend/pointset/pointhistories/service/impl/PointServiceImpl.java
@@ -3,14 +3,15 @@ package com.nhnacademy.byeol23backend.pointset.pointhistories.service.impl;
 import java.math.BigDecimal;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.nhnacademy.byeol23backend.memberset.member.domain.Member;
 import com.nhnacademy.byeol23backend.memberset.member.repository.MemberRepository;
 import com.nhnacademy.byeol23backend.pointset.pointhistories.repository.PointHistoryRepository;
-import com.nhnacademy.byeol23backend.pointset.pointhistories.device.PointDevice;
 import com.nhnacademy.byeol23backend.pointset.pointhistories.domain.PointHistory;
 import com.nhnacademy.byeol23backend.pointset.pointhistories.service.PointService;
+import com.nhnacademy.byeol23backend.pointset.pointpolicy.domain.PointPolicy;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,11 +21,10 @@ public class PointServiceImpl implements PointService {
 	private final MemberRepository memberRepository;
 	private final PointHistoryRepository pointHistoryRepository;
 
-	@Transactional
-	public void addPointsToMember(Long memberId, PointDevice pointDevice) {
-		Member member = memberRepository.findById(memberId)
-			.orElseThrow(() -> new IllegalArgumentException("Member not found with id: " + memberId));
-
-		
+	@Transactional(propagation = Propagation.MANDATORY)
+	public void addPointsToMember(Member member, PointPolicy pointPolicy) {
+		PointHistory pointHistory = new PointHistory(member, pointPolicy);
+		pointHistoryRepository.save(pointHistory);
+		member.setCurrentPoint(member.getCurrentPoint().add(pointPolicy.getSaveAmount()));
 	}
 }
