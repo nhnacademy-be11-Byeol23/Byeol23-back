8e4fdc1e096a7bb26a8b126c5cfecfc1fe9de3eb
임찬혁 <dlacksgur7@naver.com>
2025-11-10 21:58:30 +0900

    feat: 도서 기여자 연동

--- src/main/java/com/nhnacademy/byeol23backend/bookset/bookcontributor/service/BookContributorService.java
+++ src/main/java/com/nhnacademy/byeol23backend/bookset/bookcontributor/service/BookContributorService.java
@@ -1,4 +1,14 @@
 package com.nhnacademy.byeol23backend.bookset.bookcontributor.service;
 
+import java.util.List;
+
+import com.nhnacademy.byeol23backend.bookset.book.domain.Book;
+import com.nhnacademy.byeol23backend.bookset.contributor.domain.Contributor;
+
 public interface BookContributorService {
+	List<Contributor> getContributorsByBookId(Long bookId);
+
+	void createBookContributors(Book book, List<Long> ContributorIds);
+
+	void updateBookContributors(Book book, List<Long> ContributorIds);
 }
