559123c9ebe14a21b156b7ad06fc67e2be63a334
Jinsu Kim <wlstn3697@naver.com>
2025-11-10 16:30:12 +0900

    point 만듦

--- src/main/java/com/nhnacademy/byeol23backend/pointset/pointhistories/service/PointService.java
+++ src/main/java/com/nhnacademy/byeol23backend/pointset/pointhistories/service/PointService.java
@@ -1,7 +1,16 @@
 package com.nhnacademy.byeol23backend.pointset.pointhistories.service;
 
+import java.math.BigDecimal;
+
 import com.nhnacademy.byeol23backend.memberset.member.domain.Member;
+import com.nhnacademy.byeol23backend.pointset.pointhistories.domain.PointHistory;
+import com.nhnacademy.byeol23backend.pointset.pointhistories.dto.ReservedPolicy;
 import com.nhnacademy.byeol23backend.pointset.pointpolicy.domain.PointPolicy;
 
 public interface PointService {
+	PointHistory addPointsByReserved(Member member, ReservedPolicy reservedPolicy, BigDecimal orderAmount);
+
+	PointHistory addPointsWithPolicy(Member member, PointPolicy pointPolicy);
+
+	PointHistory addPointsByOrder(Member member, BigDecimal orderAmount);
 }
