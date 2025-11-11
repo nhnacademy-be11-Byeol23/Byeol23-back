08ede8996a9610a2e83fe4ccc31fbfe9d6f67eb5
박지완 <sat981023@gmail.com>
2025-11-11 16:13:24 +0900

    WIP:save

--- src/main/java/com/nhnacademy/byeol23backend/config/QuartzDbConfig.java
+++ src/main/java/com/nhnacademy/byeol23backend/config/QuartzDbConfig.java
@@ -5,11 +5,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
+@Profile("!test")
 @Configuration
 public class QuartzDbConfig {
     @Bean
