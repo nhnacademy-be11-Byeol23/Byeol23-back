2e1342b0340a70acfeabfe637ce1459925abbe30
박지완 <sat981023@gmail.com>
2025-11-12 18:19:06 +0900

    fix:엔티티 오류 수정

--- src/main/java/com/nhnacademy/byeol23backend/orderset/refundpolicy/domain/RefundPolicy.java
+++ src/main/java/com/nhnacademy/byeol23backend/orderset/refundpolicy/domain/RefundPolicy.java
@@ -1,11 +1,6 @@
 package com.nhnacademy.byeol23backend.orderset.refundpolicy.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,9 +14,8 @@ public class RefundPolicy {
 	@Column(name = "refund_policy_id")
 	private Long refundPolicyId;
 
-	@Column(name = "refund_policy_name", nullable = false, length = 30)
-	private RefundReason refundPolicyName;
-
+	@Enumerated(EnumType.STRING)
 	@Column(name = "refund_option", nullable = false, length = 30)
 	private RefundOption refundOption;
+
 }
--- src/main/java/com/nhnacademy/byeol23backend/orderset/refundpolicy/repository/RefundPolicyRepository.java
+++ src/main/java/com/nhnacademy/byeol23backend/orderset/refundpolicy/repository/RefundPolicyRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.nhnacademy.byeol23backend.orderset.refundpolicy.domain.RefundPolicy;
 
 public interface RefundPolicyRepository extends JpaRepository<RefundPolicy, Long> {
-	Optional<RefundPolicy> findFirstByOrderByChangedAtDesc();
 }
