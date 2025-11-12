f3a522fe2151516da0e974cb79d38c1a89faa616
yuhyun choi <dbgus315@gmail.com>
2025-11-12 13:50:08 +0900

    modify: 환불 정책 로직 삭제

--- src/main/java/com/nhnacademy/byeol23backend/orderset/refundpolicy/domain/RefundPolicy.java
+++ src/main/java/com/nhnacademy/byeol23backend/orderset/refundpolicy/domain/RefundPolicy.java
@@ -1,7 +1,5 @@
 package com.nhnacademy.byeol23backend.orderset.refundpolicy.domain;
 
-import java.time.LocalDateTime;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -24,23 +22,11 @@ public class RefundPolicy {
 	@Column(name = "refund_policy_name", nullable = false, length = 30)
 	private String refundPolicyName;
 
-	@Column(name = "refund_condition", nullable = false, length = 20)
-	private String refundCondition;
-
-	@Column(name = "comment", nullable = false, columnDefinition = "text")
-	private String comment;
-
-	private LocalDateTime changedAt;
-
-	private RefundPolicy(String refundPolicyName, String refundCondition, String comment, LocalDateTime changedAt) {
+	private RefundPolicy(String refundPolicyName) {
 		this.refundPolicyName = refundPolicyName;
-		this.refundCondition = refundCondition;
-		this.comment = comment;
-		this.changedAt = changedAt;
 	}
 
-	public static RefundPolicy of(String refundPolicyName, String refundCondition, String comment,
-		LocalDateTime changedAt) {
-		return new RefundPolicy(refundPolicyName, refundCondition, comment, changedAt);
+	public static RefundPolicy of(String refundPolicyName) {
+		return new RefundPolicy(refundPolicyName);
 	}
 }
