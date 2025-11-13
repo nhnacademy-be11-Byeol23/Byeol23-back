fd2781cc5b185d1470288873e359817fa9c4900d
Jinsu Kim <wlstn3697@naver.com>
2025-11-13 11:15:26 +0900

    history 조회 controller 만듦

--- src/main/java/com/nhnacademy/byeol23backend/orderset/refundpolicy/domain/RefundPolicy.java
+++ src/main/java/com/nhnacademy/byeol23backend/orderset/refundpolicy/domain/RefundPolicy.java
@@ -1,6 +1,6 @@
 package com.nhnacademy.byeol23backend.orderset.refundpolicy.domain;
 
-import jakarta.persistence.*
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
