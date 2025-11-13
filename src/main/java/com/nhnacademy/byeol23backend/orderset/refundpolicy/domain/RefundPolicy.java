551d6ffa49ea00229c1254b36ce930f199e3db85
yuhyunchoi <166400487+yuhyunchoi@users.noreply.github.com>
2025-11-13 10:15:46 +0900

    Update RefundPolicy.java

--- src/main/java/com/nhnacademy/byeol23backend/orderset/refundpolicy/domain/RefundPolicy.java
+++ src/main/java/com/nhnacademy/byeol23backend/orderset/refundpolicy/domain/RefundPolicy.java
@@ -1,13 +1,6 @@
 package com.nhnacademy.byeol23backend.orderset.refundpolicy.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
