a45efe92d9bf8d4e72155c5894051a1ccbd5ca8f
Waa-i <ramureezuns@gmail.com>
2025-11-13 10:33:35 +0900

    feat: 도서 추가 삭제 시 엘라스틱 서치에 반영

---
+++ src/main/java/com/nhnacademy/byeol23backend/bookset/book/event/BookDocumentAddEvent.java
@@ -0,0 +1,6 @@
+package com.nhnacademy.byeol23backend.bookset.book.event;
+
+import com.nhnacademy.byeol23backend.bookset.book.document.BookDocument;
+
+public record BookDocumentAddEvent(BookDocument bookDocument) {
+}
