a0c535bbdd59b01082b8cd1aded99536cd8c0a50
yuhyun choi <dbgus315@gmail.com>
2025-11-14 09:48:38 +0900

    modify: 환불 로직 수정

--- src/main/java/com/nhnacademy/byeol23backend/orderset/refund/service/impl/RefundServiceImpl.java
+++ src/main/java/com/nhnacademy/byeol23backend/orderset/refund/service/impl/RefundServiceImpl.java
@@ -22,9 +22,6 @@ import com.nhnacademy.byeol23backend.orderset.refundpolicy.domain.RefundOption;
 import com.nhnacademy.byeol23backend.orderset.refundpolicy.domain.RefundPolicy;
 import com.nhnacademy.byeol23backend.orderset.refundpolicy.exception.RefundPolicyNotFoundException;
 import com.nhnacademy.byeol23backend.orderset.refundpolicy.repository.RefundPolicyRepository;
-import com.nhnacademy.byeol23backend.pointset.pointhistories.domain.PointHistory;
-import com.nhnacademy.byeol23backend.pointset.pointhistories.exception.PointHistoryNotFoundException;
-import com.nhnacademy.byeol23backend.pointset.pointhistories.repository.PointHistoryRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -36,7 +33,6 @@ public class RefundServiceImpl implements RefundService {
 	private final RefundPolicyRepository refundPolicyRepository;
 	private final OrderRepository orderRepository;
 	private final DeliveryPolicyRepository deliveryPolicyRepository;
-	private final PointHistoryRepository pointHistoryRepository;
 
 	@Override
 	@Transactional
@@ -55,27 +51,10 @@ public class RefundServiceImpl implements RefundService {
 				() -> new RefundPolicyNotFoundException("해당 이름의 환불 종류를 찾을 수 없습니다.: " + request.refundOption()));
 		LocalDateTime now = LocalDateTime.now();
 
-		PointHistory pointHistory = pointHistoryRepository.findByOrderId(order.getOrderId())
-			.orElseThrow(
-				() -> new PointHistoryNotFoundException("포인트 적립 내역을 찾을 수 없습니다. 주문 번호: " + order.getOrderNumber()));
-
-		// 1. 포인트 적립 내역 가져옴
-		pointHistoryRepository.findByOrderId(order.getOrderId()).ifPresent(ph -> {
-			// 적립되었던 포인트를 다시 차감
-			orderedMember.updatePoint(
-				orderedMember.getCurrentPoint().subtract(ph.getPointAmount())
-			);
-		});
-
-		// 사용자가 사용한 포인트가 있는지 확인, 포인트 적립 내역에서 적립량을 가져옴
-		BigDecimal usedPoints = pointHistory.getPointAmount();
-
-		// 사용자가 주문 시에 사용한 포인트가 있으면 사용한 포인트를 돌려줌
-		if (usedPoints != null && usedPoints.compareTo(BigDecimal.ZERO) > 0) {
-			orderedMember.updatePoint(orderedMember.getCurrentPoint().add(usedPoints));
-		}
+		// [추가] 포인트 사용 내역 있으면 돌려주는 로직 추가해야 됨
 
 		// [추가] 쿠폰 사용 내역 있으면 쿠폰 다시 사용 가능 상태로 변경
+
 		BigDecimal refundAmount = null;
 		if (request.refundOption().equals(RefundOption.BREAK)) { // 파손 파지
 			refundAmount = getRefundFee(actualOrderPrice, new BigDecimal(0L)); // 배송비 0원
--- src/main/java/com/nhnacademy/byeol23backend/pointset/pointhistories/repository/PointHistoryRepository.java
+++ src/main/java/com/nhnacademy/byeol23backend/pointset/pointhistories/repository/PointHistoryRepository.java
@@ -1,7 +1,6 @@
 package com.nhnacademy.byeol23backend.pointset.pointhistories.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,6 +9,4 @@ import com.nhnacademy.byeol23backend.pointset.pointhistories.domain.PointHistory
 
 public interface PointHistoryRepository extends JpaRepository<PointHistory, Long> {
 	List<PointHistory> findAllByMemberId(Member member);
-
-	Optional<PointHistory> findByOrderId(Long orderId);
 }
