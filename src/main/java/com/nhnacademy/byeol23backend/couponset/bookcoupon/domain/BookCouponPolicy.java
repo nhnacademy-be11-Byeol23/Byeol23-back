e4c4e4de3de69bee5ea0677cb067748157bd59c9
박지완 <sat981023@gmail.com>
2025-11-19 11:10:38 +0900

    WIP:save

--- src/main/java/com/nhnacademy/byeol23backend/couponset/bookcoupon/domain/BookCouponPolicy.java
+++ src/main/java/com/nhnacademy/byeol23backend/couponset/bookcoupon/domain/BookCouponPolicy.java
@@ -3,6 +3,7 @@ package com.nhnacademy.byeol23backend.couponset.bookcoupon.domain;
 import com.nhnacademy.byeol23backend.bookset.book.domain.Book;
 import com.nhnacademy.byeol23backend.couponset.couponpolicy.domain.CouponPolicy;
 
+import com.nhnacademy.byeol23backend.couponset.couponpolicy.domain.dto.CouponPolicyCreateRequest;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -13,7 +14,10 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
 @Entity
 @Table(name = "book_coupon_policy")
 public class BookCouponPolicy {
@@ -22,12 +26,23 @@ public class BookCouponPolicy {
 	@Column(name = "book_coupon_id")
 	private Long bookCouponId;
 
+	@Setter
 	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "coupon_policy_id", nullable = false)
 	private CouponPolicy couponPolicy;
 
+	@Setter
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "book_id", nullable = false)
 	private Book book;
+
+	private BookCouponPolicy(CouponPolicy couponPolicy, Book book) {
+		this.couponPolicy = couponPolicy;
+		this.book = book;
+	}
+
+	public static BookCouponPolicy createFromDto(CouponPolicy couponPolicy, Book book){
+		return new BookCouponPolicy(couponPolicy, book);
+	}
 
 }
