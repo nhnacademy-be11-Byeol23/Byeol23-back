344704e7a30405af76083b8eb6d30482f79b5837
박지완 <sat981023@gmail.com>
2025-11-20 14:36:55 +0900

    modify:CouponPolicy 엔티티 변경

--- src/main/java/com/nhnacademy/byeol23backend/couponset/couponpolicy/domain/CouponPolicy.java
+++ src/main/java/com/nhnacademy/byeol23backend/couponset/couponpolicy/domain/CouponPolicy.java
@@ -16,37 +16,40 @@ import lombok.NoArgsConstructor;
 @Table(name = "coupon_policy")
 @NoArgsConstructor
 public class CouponPolicy {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "coupon_policy_id")
-	private Long couponPolicyId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "coupon_policy_id")
+    private Long couponPolicyId;
 
-	@Column(name = "coupon_policy_name", nullable = false, length = 30)
-	private String couponPolicyName;
+    @Column(name = "coupon_policy_name", nullable = false, length = 30)
+    private String couponPolicyName;
 
-	@Column(name = "criterion_price", nullable = false, precision = 10)
-	private BigDecimal criterionPrice;
+    @Column(name = "criterion_price", nullable = false, precision = 10)
+    private BigDecimal criterionPrice;
 
-	@Column(name = "discount_rate")
-	private Integer discountRate;
+    @Column(name = "discount_rate")
+    private Integer discountRate;
 
-	@Column(name = "discount_limit", precision = 10)
-	private BigDecimal discountLimit;
+    @Column(name = "discount_limit", precision = 10)
+    private BigDecimal discountLimit;
 
-	@Column(name = "discount_amount", precision = 10)
-	private BigDecimal discountAmount;
+    @Column(name = "discount_amount", precision = 10)
+    private BigDecimal discountAmount;
 
-	@Column(name = "is_active", nullable = false)
-	private Boolean isActive;
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
 
-	public CouponPolicy(String couponPolicyName, BigDecimal criterionPrice, Integer discountRate,
-		BigDecimal discountLimit,
-		BigDecimal discountAmount) {
-		this.couponPolicyName = couponPolicyName;
-		this.criterionPrice = criterionPrice;
-		this.discountRate = discountRate;
-		this.discountLimit = discountLimit;
-		this.discountAmount = discountAmount;
-		this.isActive = true;
-	}
+    @Column(name = "coupon_policy_type", nullable = false)
+    private String couponPolicyType;
+
+    public CouponPolicy(String couponPolicyName, BigDecimal criterionPrice, Integer discountRate,
+                        BigDecimal discountLimit, BigDecimal discountAmount, String couponPolicyType) {
+        this.couponPolicyName = couponPolicyName;
+        this.criterionPrice = criterionPrice;
+        this.discountRate = discountRate;
+        this.discountLimit = discountLimit;
+        this.discountAmount = discountAmount;
+        this.isActive = true;
+        this.couponPolicyType = couponPolicyType;
+    }
 }
