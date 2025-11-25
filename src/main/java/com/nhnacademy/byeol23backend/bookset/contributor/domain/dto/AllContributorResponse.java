2ea7ac65bde3564c268e320eadd86255faeb6152
hyungwooro <hyungwoo.ro@gmail.com>
2025-11-25 16:25:48 +0900

    modify: AllContributorResponse의 contritbutorRole을 문자열로 재수정

--- src/main/java/com/nhnacademy/byeol23backend/bookset/contributor/domain/dto/AllContributorResponse.java
+++ src/main/java/com/nhnacademy/byeol23backend/bookset/contributor/domain/dto/AllContributorResponse.java
@@ -6,12 +6,9 @@ import com.nhnacademy.byeol23backend.bookset.contributor.domain.ContributorRole;
 public record AllContributorResponse(
 	Long contributorId,
 	String contributorName,
-	ContributorRole contributorRole
+	String contributorRole
 ) {
 	public AllContributorResponse(Contributor contributor){
-		this(contributor.getContributorId(), contributor.getContributorName(), contributor.getContributorRole());
-	}
-	public AllContributorResponse(Long contributorId, String contributorName, String contributorRole){
-		this(contributorId, contributorName, ContributorRole.valueOf(contributorRole));
+		this(contributor.getContributorId(), contributor.getContributorName(), contributor.getContributorRole().getLabel());
 	}
 }
