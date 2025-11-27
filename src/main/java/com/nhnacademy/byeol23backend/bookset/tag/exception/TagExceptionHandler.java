37aa1c8f06abc6c64ff3c291f229bfb2ce426c2d
hyungwooro <hyungwoo.ro@gmail.com>
2025-11-27 15:58:23 +0900

    feat: exception handling

--- src/main/java/com/nhnacademy/byeol23backend/bookset/tag/exception/TagExceptionHandler.java
+++ src/main/java/com/nhnacademy/byeol23backend/bookset/tag/exception/TagExceptionHandler.java
@@ -1,22 +1,32 @@
 package com.nhnacademy.byeol23backend.bookset.tag.exception;
 
+import java.time.LocalDateTime;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.nhnacademy.byeol23backend.commons.exception.ErrorResponse;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestControllerAdvice
 public class TagExceptionHandler {
 
-	@ExceptionHandler(TagAlreadyExistsException.class)
-	public ResponseEntity<ErrorResponse> handleTagNotFoundException(TagNotFoundException e) {
-		ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+	@ExceptionHandler(TagNotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleTagNotFoundException(TagNotFoundException e, HttpServletRequest request) {
+		ErrorResponse error = new ErrorResponse(
+			HttpStatus.NOT_FOUND.value(),
+			e.getMessage(),
+			request.getRequestURI(),
+			LocalDateTime.now());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
 
 	@ExceptionHandler(TagAlreadyExistsException.class)
-	public ResponseEntity<ErrorResponse> handleTagAlreadyExistsException(TagAlreadyExistsException e) {
-		ErrorResponse error = new ErrorResponse(HttpStatus.CONFLICT.value(), e.getMessage());
+	public ResponseEntity<ErrorResponse> handleTagAlreadyExistsException(TagAlreadyExistsException e, HttpServletRequest request) {
+		ErrorResponse error = new ErrorResponse(HttpStatus.CONFLICT.value(), e.getMessage(), request.getRequestURI(), LocalDateTime.now());
 		return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
 	}
 }
