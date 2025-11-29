756bfb84381d942399450cb5c49a24fc99e8d3fe
Jinsu Kim <wlstn3697@naver.com>
2025-11-29 11:15:39 +0900

    fix: review

--- src/main/java/com/nhnacademy/byeol23backend/pointset/pointpolicy/dto/ReservedPolicy.java
+++ src/main/java/com/nhnacademy/byeol23backend/pointset/pointpolicy/dto/ReservedPolicy.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum ReservedPolicy {//enum의 내용 삭제하지 말것 추가만 하세요
 	ORDER("주문"),
-	REVIEW("리뷰"),
+	REVIEW_WITH_IMAGE("리뷰"),
+	REVIEW_WITHOUT_IMAGE("이미지 리뷰"),
 	REGISTER("회원가입"),
 	UNKNOWN("알수없음"),
 	CANCEL("취소");
